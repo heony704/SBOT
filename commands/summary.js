ce44d8e9ddcb5f249fe93249c260f015c812cc66
SeungHe0n <014sh@naver.com>
2021-09-09 10:51:11 +0900

    summary 자정에 뜨도록 수정

--- commands/summary.js
+++ commands/summary.js
@@ -1,9 +1,8 @@
 
 const { when, howlong, whatDate } = require('./convertTime');
 
-const matchDate = function (date) {
-    // const now = new Date();
-    return (date.getDate() === new Date().getDate());
+const matchTime = function (date) {
+    return (date.getMinutes() === new Date().getMinutes());
 }
 
 const setSummary = function (message, intervalList, userList, goalHour) {
@@ -15,44 +14,46 @@ const setSummary = function (message, intervalList, userList, goalHour) {
     }
 
     let comment = `해당 채널에 **하루 공부시간 요약**이 설정되었습니다.\n`;
-    comment += `목표 시간을 달성하면 따봉:thumbsup: 을 , 달성하지 못한다면 벽돌:bricks: 을 받습니다.`
+    comment += `목표 시간을 달성하면 따봉:thumbsup:을 , 달성하지 못한다면 벽돌:bricks:을 받습니다.`
     message.channel.send(comment);
 
     const time = new Date();
 
     const interval = setInterval( function () {
 
-        // matchDate, matchTime 변경
-        if (!matchDate(time)) {
+        if (!matchTime(time)) {
 
-            let comment = `:mega:  ${whatDate(time)}\n`;
             const now = new Date();
             
-            if ( userList.size === 0 ) {
-                comment += `- 아직 참여한 사용자가 없습니다 -`;
-                message.channel.send(comment);
-            } else {
-                userList.forEach((user, id) => {
-                    if (user.startTime) {
-                        user.totalTime.setTime(user.totalTime.getTime() + (now.getTime() - user.startTime.getTime()));
-                        user.startTime = now;
-                    }
+            if (time.getMinutes() === 59 && time.getHours() === 14) {
 
-                    comment += `<@${id}> `;
-                    if (user.totalTime.getHours() >= goalHour) {
-                        comment += `${howlong(user.totalTime)}  :thumbsup:\n`;
-                    } else {
-                        comment += `${howlong(user.totalTime)}  :bricks:\n`;
-                    }
-                    user.totalTime = new Date(2021, 0);
-                });
-            
-                message.channel.send(comment);
+                let comment = `:mega:  ${whatDate(time)}\n`;
+                
+                if ( userList.size === 0 ) {
+                    comment += `- 아직 참여한 사용자가 없습니다 -`;
+                    message.channel.send(comment);
+                } else {
+                    userList.forEach((user, id) => {
+                        if (user.startTime) {
+                            user.totalTime.setTime(user.totalTime.getTime() + (now.getTime() - user.startTime.getTime()));
+                            user.startTime = now;
+                        }
+
+                        comment += `<@${id}> `;
+                        if (user.totalTime.getHours() >= goalHour) {
+                            comment += `${howlong(user.totalTime)}  :thumbsup:\n`;
+                        } else {
+                            comment += `${howlong(user.totalTime)}  :bricks:\n`;
+                        }
+                        user.totalTime = new Date(2021, 0);
+                    });
+                
+                    message.channel.send(comment);
+                }
             }
-            
+
             // 시간 초기화
-            // time.setHours(now.getHours());
-            time.setDate(now.getDate());
+            time.setTime(now.getTime());
         }
     }, 900);
     

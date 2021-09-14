dd6fa1226293814911b06cacc465ba63f6e5a6c0
SeungHe0n <014sh@naver.com>
2021-09-14 22:24:16 +0900

    today 명령어 기능 없애지는 않고, 보여주지 않게 수정

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,6 @@ const help = function (channel) {
     comment += `start , s  스톱워치를 시작합니다.\n`;
     comment += `pause , p  스톱워치를 멈춥니다.\n`;
     comment += `hours , h  오늘 얼마나 공부했는지 알려줍니다.\n`;
-    comment += `today , t  오늘의 날짜를 알려줍니다.\n`;
     comment += `goal  , g  목표 공부시간을 알려줍니다.\n`;
     comment += `\`\`\``;
     
